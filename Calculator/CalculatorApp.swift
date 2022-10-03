2917b25851febef7c70567717b3244d183ef8605
Ramenskii Kirill <ramenskii-kr@mail.ru>
2022-10-04 00:35:53 +0700

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Кирилл Раменский on 04.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
