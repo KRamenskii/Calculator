4d7696ff910100d5414d59af5b43b1be9c0dfdd1
Ramenskii Kirill <ramenskii-kr@mail.ru>
2022-10-07 17:49:24 +0700

    CL-1
    Adjusted the size and padding for the buttons.

---
+++ Calculator/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  Calculator
+//
+//  Created by Кирилл Раменский on 07.10.2022.
+//
+
+import Foundation
+import CoreGraphics
+
+struct Constants {
+    static let padding: CGFloat = 12
+}
--- Calculator/Styles/CalculatorButtonStyle.swift
+++ Calculator/Styles/CalculatorButtonStyle.swift
@@ -12,11 +12,13 @@ struct CalculatorButtonStyle: ButtonStyle {
     var size: CGFloat
     var backgroundColor: Color
     var foregroundColor: Color
+    var isWide: Bool = false
     
     func makeBody(configuration: Configuration) -> some View {
         configuration.label
             .font(.system(size: 32, weight: .medium))
             .frame(width: size, height: size)
+            .frame(maxWidth: isWide ? .infinity : size, alignment: .leading)
             .background(backgroundColor)
             .foregroundColor(foregroundColor)
             .overlay {
@@ -36,7 +38,8 @@ struct CalculatorButtonStyle_Previews: PreviewProvider {
             .buttonStyle(CalculatorButtonStyle(
                 size: 80,
                 backgroundColor: buttonType.backgroundColor,
-                foregroundColor: buttonType.foregroundColor)
+                foregroundColor: buttonType.foregroundColor,
+                isWide: buttonType == .digit(.zero))
             )
     }
 }
